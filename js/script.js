27e96bcf73e3a3adeba5718515bafe6f552f379a
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-02 21:01:07 +0300

    Cleaning the code

--- js/script.js
+++ js/script.js
@@ -1,48 +1,47 @@
 $(document).ready(function () {
-    var inputval = $(".form-control").val();
-    let arrayOfTodos = [];
-    $(".add").click(function () {
-        var inputval = $(".form-control").val();
-        if (inputval != "") {
-            addTodoToArray(inputval);
-                //removing text after we are done writing
-                $(".form-control").val("");
-        }
-    });
-    function addTodoToArray(todotext){
-        const todo = {
-            id: Math.floor(Math.random()*1000),
-            title: "---Task---",
-            description: todotext,
-            isDone : false,
-            point: 1,
-            date : Date.now(),
-        };
+  let arrayOfTodos = [];
 
-        //now pushing todo into the array of todos
-        arrayOfTodos.push(todo);
-        //now displaying the tasks
-        displayTodos(arrayOfTodos);
-
+  //Add todo
+  $(".add").click(function () {
+    if ($(".form-control").val() !== "") {
+      addTodoToArray($(".form-control").val());
+      //removing text after we are done writing
+      $(".form-control").val("");
     }
-    
-    function displayTodos(arrayOfTodos){
-        //Emptying todo list ul
-        $(".todo-list").innerHTML = "";
-        //now looping over the array
-        arrayOfTodos.forEach((todo) => {
-         let div = document.createElement("div");
-         div.className = "todo";
-         div.appendChild(document.createTextNode(todo.description));
-         
-         //create delete button
-         
-         let span = document.createElement("span");
-         span.className = "remove";
-         span.append(document.createTextNode("Delete"));
-         //Append Button to main div
-         div.append(span);
-         console.log(div);
-        })
-    } 
+  });
+  function addTodoToArray(todotext) {
+    const todo = {
+      id: Math.floor(Math.random() * 1000),
+      title: "---Task---",
+      description: todotext,
+      isDone: false,
+      point: 1,
+      date: Date.now(),
+    };
+
+    //now pushing todo into the array of todos
+    arrayOfTodos.push(todo);
+    //now displaying the tasks
+    displayTodos(arrayOfTodos);
+  }
+
+  function displayTodos(arrayOfTodos) {
+    //Emptying todo list div
+    $(".todos").innerHTML = "";
+    //now looping over the array
+    arrayOfTodos.forEach((todo) => {
+      //create main div
+      let div = document.createElement("div");
+      div.className = "todo";
+      div.setAttribute("data-id", todo.id);
+      div.appendChild(document.createTextNode(todo.description));
+      //create delete button
+      let span = document.createElement("span");
+      span.className = "del";
+      span.appendChild(document.createTextNode("delete"));
+      //append button to main div
+      div.appendChild(span);
+      console.log(div);
+    });
+  }
 });
