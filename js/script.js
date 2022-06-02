5a757cd8396bbddc3cea3cde9eacb4032b893200
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-02 19:44:28 +0300

    Adding function addTodoToArray

--- js/script.js
+++ js/script.js
@@ -9,6 +9,21 @@ $(document).ready(function () {
                 $(".form-control").val("");
         }
     });
-  
+    function addTodoToArray(todotext){
+        const todo = {
+            id: Math.floor(Math.random()*1000),
+            title: "---Task---",
+            description: todotext,
+            isDone : false,
+            point: 1,
+            date : Date.now(),
+        };
+
+        //now pushing todo into the array of todos
+        arrayOfTodos.push(todo);
+        //now displaying the tasks
+        displayTodos(arrayOfTodos);
+
+    }
     
 });
